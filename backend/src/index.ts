dababbca00cb0b85e8abf53aa4215c89134951da
klucky07 <khanlucky2020@gmail.com>
2024-08-19 09:14:39 +0530

    db added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,21 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post("/api/v1/signup",)
-app.post("/api/v1/signin")
+app.post("/api/v1/signup",(c)=>{
+  return c.text("hello")
+})
+app.post("/api/v1/signin",(c)=>{
+  return c.text("hello")
+})
+app.post("/api/v1/blog",(c)=>{
+  return c.text("hello")
+})
+app.put("/api/v1/blog",(c)=>{
+  return c.text("hello")
+})
+app.get("/api/v1/blog/:id",(c)=>{
+  return c.text("hello")
+})
+
 
 export default app
